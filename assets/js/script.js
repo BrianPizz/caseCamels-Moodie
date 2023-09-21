cbc4cef8dd00d55914fe1fa6b7007f2c3f3fb6a4
Wgomez1987 <gomez.william.a1@gmail.com>
2023-09-21 13:00:08 -0700

    food variables added

---
+++ assets/js/script.js
@@ -0,0 +1,57 @@
+
+// API Links:
+var cuisine = ""
+var genre = ""
+// find endpoints for recipes
+// https://yummly2.p.rapidapi.com/tags/list
+
+var american = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-american"
+var bbq = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-barbecue"
+var asian = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-asian"
+var italian = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-italian"
+var mexican = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-mexican"
+var french = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-french"
+var southWestern = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-southwestern"
+var southern = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-southern"
+var indian = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-indian"
+var english = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-english"
+var chinese = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-chinese"
+var medit = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-mediterranean"
+var spanish = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-spanish"
+var greek = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-greek"
+var thai = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-thai"
+var cajun = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-cajun)"
+var irish = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-irish)"
+var german = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-german)"
+var morac = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-moroccan)"
+var japanese = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-japanese)"
+var cuban = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-cuban)"
+var hawi = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-hawaiian)"
+var swedish = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-swedish)"
+var port = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-portuguese)"
+var hungarian = "list.recipe.search_based:fq:attribute_s_mv:(cuisine\^cuisine\-hungarian)"
+
+
+
+// https://ott-details.p.rapidapi.com/advancedsearch?min_imdb=8&max_imdb=10&type=movie&page=1
+//
+//
+    // start year
+    // end yearmin_imdb
+    // max_imdb
+    // genre
+    // language
+    // type
+    // sort
+    // page
+
+    var startYear = ""
+    var endYear = ""
+    var genre = ""
+    var language = ""
+    var sortList = ""
+
+
+
+
+
