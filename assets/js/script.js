edc25cd92747be42becdb193032a138e6eec2756
BrianPizz <bpizzimentijr@outlook.com>
2023-09-21 17:11:58 -0400

    added carousel for movie results

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+//bulma carousel
+bulmaCarousel.attach('#slider', {
+    slidesToScroll: 1,
+    slidesToShow: 1,
+    infinite: true,
+    icons: { previous: "<span style='color:orange;font-weight:800'>&lt;</span>", next: "<span style='color:orange;font-weight:800'>&gt;</span>"}
+  });
