24929b4c187f065281d98db411be98821828cdce
BrianPizz <bpizzimentijr@outlook.com>
2023-09-22 13:39:54 -0400

    removed remaining carousel elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,15 +79,8 @@ $('.langBtn').on('click', function(){
 // Event Listeners
 startBtnEl.on('click', startSearch)
 
-//bulma carousel
-bulmaCarousel.attach('#slider', {
-    slidesToScroll: 1,
-    slidesToShow: 1,
-    infinite: true,
-    icons: { previous: "<span style='color:orange;font-weight:800'>&lt;</span>", next: "<span style='color:orange;font-weight:800'>&gt;</span>" }
-});
 
-// carousel is hidden
+// intial load
 function initLoad(){
     resultPageEl.addClass('hidden')
 }
